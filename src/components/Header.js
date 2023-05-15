66c1ed055512095c929e42ff27db5769388b3b9d
Gustavo-Oliveira7 <gustavo.h.oliveira7531@gmail.com>
2023-05-15 11:16:51 -0300

    adicionando o cv no botao do header

--- src/components/Header.js
+++ src/components/Header.js
@@ -21,7 +21,7 @@ class Header extends Component {
           <UilGithub size='50' color="#000000"/>
         </a>
         <a
-          href="https://www.linkedin.com/in/gustavo-oliveira7/"
+          href="https://drive.google.com/file/d/1oJhDhzIggiQd_jQ5atQg_an6GUB0sEJY/view?usp=sharing"
           target="_blank"
           className="navItem" rel="noreferrer">
           <UilFile size='50' color="#000000"/>
